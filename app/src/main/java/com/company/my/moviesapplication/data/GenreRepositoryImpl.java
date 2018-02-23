cc0429b0126c659998369b056b01efd8bbb1f984
Oleja <oleeja@gmail.com>
2018-02-23 22:37:12 +0200

    Made right filtering

--- app/src/main/java/com/company/my/moviesapplication/data/GenreRepositoryImpl.java
+++ app/src/main/java/com/company/my/moviesapplication/data/GenreRepositoryImpl.java
@@ -4,10 +4,8 @@ import android.text.TextUtils;
 
 import com.company.my.moviesapplication.domain.genre.GenreModel;
 import com.company.my.moviesapplication.domain.genre.GenreRepository;
-import com.company.my.moviesapplication.domain.movie.MovieModel;
 import com.company.my.moviesapplication.utils.MockMovieData;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import javax.inject.Inject;
@@ -42,27 +40,24 @@ public class GenreRepositoryImpl implements GenreRepository {
     @Override
     public void setFilter(String filter) {
         List<GenreModel> genreModels = MockMovieData.getMockFilms();
+
         Flowable.fromIterable(genreModels)
-                .flatMap(genreModel -> {
-                            List<MovieModel> movieModels = new ArrayList<>();
-                            return Flowable.fromIterable(genreModel.getMovies())
-                                    .filter(movieModel -> movieModel.getName().toLowerCase().contains(filter.toLowerCase()))
-                                    .map(movieModel -> {
-                                        movieModels.add(movieModel);
-                                        return genreModel;
-                                    })
-                                    .doOnComplete(() -> {
-                                        genreModel.setMovies(movieModels);
-                                    })
-                                    .distinctUntilChanged();
-                        }
-                )
-                .toList().toFlowable()
-                .subscribe(processor::onNext, throwable -> {
-                    // TODO: 22.02.2018 handle error
-                }, () -> {
-                    // TODO: 22.02.2018 handle completable
-                });
+                .flatMap(genreModel
+                        -> Flowable.fromIterable(genreModel.getMovies())
+                        .filter(movieModel -> movieModel.getName().toLowerCase().contains(filter.toLowerCase()))
+                        .toList().toFlowable()
+                        .flatMap(movieModels -> {
+                            genreModel.setMovies(movieModels);
+                            return Flowable.just(genreModel);
+                        })).toList().toFlowable()
+                .subscribe(t -> {
+                            processor.onNext(t);
+                        },
+                        throwable -> {
+                            // TODO: 23.02.2018 handle error
+                        }, () -> {
+                            // TODO: 23.02.2018 handle completable
+                        });
 
 
     }
